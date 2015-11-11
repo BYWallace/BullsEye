96bdc4cbee5759d13fed2fdc80c88ed96ede6e39
Brett Wallace <bwallace90@gmail.com>
2015-11-11 14:54:29 -0500

    Game generates random target number and displays it

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     var currentValue: Int = 0
+    var targetValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         currentValue = lroundf(slider.value)
+        targetValue = 1 + Int(arc4random_uniform(100))
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +26,7 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         let message = "The value of the slider is: \(currentValue)"
+                    + "\nThe target value is: \(targetValue)"
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
         
